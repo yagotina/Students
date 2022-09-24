6c63732edfea70af466cb9b33c0511df40df9a74
Kristina Yagotina <kris.yagotina@gmail.com>
2022-09-24 18:29:49 +0200

    first commit

---
+++ gulpfile.js
@@ -0,0 +1,34 @@
+const { src, dest, series, parallel, watch } = require('gulp');
+const del = require('del');
+const browserSync = require('browser-sync').create();
+
+const clean = () => del(["dist"]);
+
+const transferFiles = cb => {
+    src('src/*.html')
+      .pipe(dest('dist'));
+
+    src(['src/css/style.css'])
+      .pipe(dest('dist/css/'));
+  
+    src(['src/js/index.js'])
+      .pipe(dest('dist/js/'));
+  
+    cb();
+};
+
+const watchFiles = cb => {
+    watch(["./src/*/*"]).on('change', browserSync.reload);
+
+    cb();
+};
+
+const browser = () => {
+    browserSync.init({
+        server: {
+            baseDir: "./src"
+        }
+    });
+};
+
+exports.default = series(clean, transferFiles, parallel(watchFiles, browser));
